399df2060a67cdc452344b583594bed8952b9d64
Omar Acuña Ascencio <omar_ivan_a@hotmail.com>
2020-05-22 22:51:28 -0600

    Se termino profesores

--- src/app/profesor/profesor.component.ts
+++ src/app/profesor/profesor.component.ts
@@ -1,11 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Profesor } from './profesor'
+
 @Component({
   selector: 'app-profesor',
   templateUrl: './profesor.component.html',
   styleUrls: ['./profesor.component.css']
 })
 export class ProfesorComponent implements OnInit {
+
+
+  profesor : any = [
+
+    new Profesor(1, 'Alfonso Barajas'),
+    new Profesor(2, 'Ivonne Siqueiros'),
+    new Profesor(3, 'Silvia Benitez'),
+    new Profesor(4, 'Alejandro Enriquez')
+    
+
+  ];
 
   constructor() { }
 
