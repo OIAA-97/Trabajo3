77c3863989ac445a7b4a66835fe9b67a58a3f225
Omar Acuña Ascencio <omar_ivan_a@hotmail.com>
2020-05-22 22:24:57 -0600

    Esta listo carrera

--- src/app/carrera/carrera.component.ts
+++ src/app/carrera/carrera.component.ts
@@ -1,11 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Carrera } from './carrera';
+
 @Component({
   selector: 'app-carrera',
   templateUrl: './carrera.component.html',
   styleUrls: ['./carrera.component.css']
 })
 export class CarreraComponent implements OnInit {
+
+  carrera : any = [
+
+    new Carrera(1, 'Tecnologias de la informacion y comunicacion'),
+    new Carrera(2, 'Desarrollo de negocios')
+
+  ];
+
+
+
 
   constructor() { }
 
---
+++ src/app/carrera/carrera.ts
@@ -0,0 +1,6 @@
+export class Carrera {
+
+    constructor (public id : number, public nombre : string){}
+
+
+}
