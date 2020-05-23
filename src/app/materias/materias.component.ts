e6f5788682e86d6cb239e1cd2528629d17aa3ceb
Omar Acuña Ascencio <omar_ivan_a@hotmail.com>
2020-05-22 23:06:05 -0600

    Se termino materias

--- src/app/materias/materias.component.ts
+++ src/app/materias/materias.component.ts
@@ -1,11 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Materia } from './materia'
+
 @Component({
   selector: 'app-materias',
   templateUrl: './materias.component.html',
   styleUrls: ['./materias.component.css']
 })
 export class MateriasComponent implements OnInit {
+
+  materia : any = [
+
+    new Materia(1, 'Desarrollo De Aplicacion III'),
+    new Materia(2, 'Integradora II'),
+    new Materia(3, 'Administracion de proyecto'),
+    new Materia(4, 'Ingles V')
+    
+
+  ];
 
   constructor() { }
 
