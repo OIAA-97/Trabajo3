7524f44de4b4c741ca68a4c2efb744c9ab1e856f
Omar Acuña Ascencio <omar_ivan_a@hotmail.com>
2020-05-22 20:48:45 -0600

    Se agrega modulo de Carrera

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,17 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+import { CarreraComponent } from './carrera/carrera.component';
+
+//Arreglo de las rutas
+const routes: Routes = [
+
+
+  { path: 'carrera', component: CarreraComponent }
+
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CarreraComponent } from './carrera/carrera.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CarreraComponent
   ],
   imports: [
     BrowserModule,
